d70b9012a9e7d51d2c9302874c2aabd004760a16
deniznida <denizunat@hotmail.com>
2015-02-17 13:17:27 -0500

    done

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -7,3 +7,24 @@
 # 10000 = Reverse the order of the operations in the secret handshake.
 
 # Write your code here.
+class SecretHandshake
+  attr_reader :binary, :solution
+
+  def initialize (binary)
+    @binary = binary
+    @solution = []
+  end
+
+  def commands
+    binary.reverse.each_char.with_index(0) do |char, i|
+      if char == "1"
+        solution << "wink" if i == 0
+        solution << "double blink" if i == 1
+        solution << "close your eyes" if i == 2
+        solution << "jump" if i == 3
+      end
+    end
+
+    (binary.length > 4 && binary[-5] == "1") ? solution.reverse! : solution
+  end
+end
